d467844d4a22201448fcbf210a2b5aaeed7d8cc0
Kibrael Roell <kibrael.roell@cfpb.gov>
2018-08-31 15:58:57 -0400

    fix bug in file write

--- hmda_load_scripts/write_data_paths.py
+++ hmda_load_scripts/write_data_paths.py
@@ -6,10 +6,9 @@ from os.path import isfile, join
 
 mypath = "SQL/"
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-ts_files = [f for f in onlyfiles if f[-11:-9]=="ts"]
-lar_files = [f for f in onlyfiles if f[-12:-9]=="lar"]
-panel_files = [f for f in onlyfiles if f[-14:-9]=="panel"]
-ts_files = ["SQL/"+f for f in ts_files]
+ts_files = [mypath+f for f in onlyfiles if f[-11:-9]=="ts"]
+lar_files = [mypath+f for f in onlyfiles if f[-12:-9]=="lar"]
+panel_files = [mypath+f for f in onlyfiles if f[-14:-9]=="panel"]
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 dir_path = dir_path[:-39]
@@ -19,10 +18,15 @@ lar_replacement_text = dir_path
 panel_replacement_text = dir_path
 text_to_search = '{data_path}'
 
+
 with fileinput.FileInput(ts_files, inplace=True, backup='') as sql_file:	
 	for line in sql_file:
 		print(line.replace(text_to_search, ts_replacement_text), end='')
 
-#with fileinput.FileInput(filename, inplace=True, backup=None) as file:
-#    for line in file:
-#        print(line.replace(text_to_search, replacement_text), end='')
+with fileinput.FileInput(lar_files, inplace=True, backup='') as sql_file:	
+	for line in sql_file:
+		print(line.replace(text_to_search, lar_replacement_text), end='')
+
+with fileinput.FileInput(panel_files, inplace=True, backup='') as sql_file:	
+	for line in sql_file:
+		print(line.replace(text_to_search, panel_replacement_text), end='')
