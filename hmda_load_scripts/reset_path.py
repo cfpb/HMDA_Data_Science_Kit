a8004b315e2849856fdc10e25894643babf4ae34
Kibrael Roell <kibrael.roell@cfpb.gov>
2018-08-31 15:46:27 -0400

    reorder commands

--- hmda_load_scripts/reset_path.py
+++ hmda_load_scripts/reset_path.py
@@ -8,11 +8,12 @@ onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 ts_files = [mypath+f for f in onlyfiles if f[-11:-9]=="ts"]
 lar_files = [mypath+f for f in onlyfiles if f[-12:-9]=="lar"]
 panel_files = [mypath+f for f in onlyfiles if f[-14:-9]=="panel"]
-replacement_text = '{data_path}'
+
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 dir_path = dir_path[:-39]
 text_to_search = dir_path
+replacement_text = '{data_path}'
 
 with fileinput.FileInput(ts_files, inplace=True, backup='') as sql_file:	
 	for line in sql_file:
