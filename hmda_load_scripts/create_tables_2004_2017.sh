db9b67f527a956a8274fd067278516f95e21efe0
Kibrael Roell <kibrael.roell@cfpb.gov>
2018-08-31 16:15:14 -0400

    add all sql commands for years 2004-2017

--- hmda_load_scripts/create_tables_2004_2017.sh
+++ hmda_load_scripts/create_tables_2004_2017.sh
@@ -12,12 +12,56 @@ export PGDATABASE=${PGDATABASE-postgres} #initial connection database, HMDA db w
 export PGUSER=${PGUSER-postgres} #enter your PG username
 export PGPASSWORD=${PGPASSWORD-} #PG password
 
+python3 write_data_paths.py
 
-echo "creating HMDA table for 2004"
+echo "creating HMDA tables for 2004-2017"
 psql $PGUSER $PGDATABASE << EOF
 	\c hmda;
 	set schema 'hmda_public';
+	\i 'SQL/create_and_load_panel_2004.sql'
+	\i 'SQL/create_and_load_panel_2005.sql'
+	\i 'SQL/create_and_load_panel_2006.sql'
+	\i 'SQL/create_and_load_panel_2007.sql'
+	\i 'SQL/create_and_load_panel_2008.sql'
+	\i 'SQL/create_and_load_panel_2009.sql'
+	\i 'SQL/create_and_load_panel_2010.sql'
+	\i 'SQL/create_and_load_panel_2011.sql'
+	\i 'SQL/create_and_load_panel_2012.sql'
+	\i 'SQL/create_and_load_panel_2013.sql'
+	\i 'SQL/create_and_load_panel_2014.sql'
+	\i 'SQL/create_and_load_panel_2015.sql'
+	\i 'SQL/create_and_load_panel_2016.sql'
+	\i 'SQL/create_and_load_panel_2017.sql'
+
+	\i 'SQL/create_and_load_ts_2004.sql'
+	\i 'SQL/create_and_load_ts_2005.sql'
+	\i 'SQL/create_and_load_ts_2006.sql'
+	\i 'SQL/create_and_load_ts_2007.sql'
+	\i 'SQL/create_and_load_ts_2008.sql'
+	\i 'SQL/create_and_load_ts_2009.sql'
+	\i 'SQL/create_and_load_ts_2010.sql'
+	\i 'SQL/create_and_load_ts_2011.sql'
+	\i 'SQL/create_and_load_ts_2012.sql'
+	\i 'SQL/create_and_load_ts_2013.sql'
+	\i 'SQL/create_and_load_ts_2014.sql'
+	\i 'SQL/create_and_load_ts_2015.sql'
+	\i 'SQL/create_and_load_ts_2016.sql'
+	\i 'SQL/create_and_load_ts_2017.sql'
+
 	\i 'SQL/create_and_load_lar_2004.sql'
+	\i 'SQL/create_and_load_lar_2005.sql'
+	\i 'SQL/create_and_load_lar_2006.sql'
+	\i 'SQL/create_and_load_lar_2007.sql'
+	\i 'SQL/create_and_load_lar_2008.sql'
+	\i 'SQL/create_and_load_lar_2009.sql'
+	\i 'SQL/create_and_load_lar_2010.sql'
+	\i 'SQL/create_and_load_lar_2011.sql'
+	\i 'SQL/create_and_load_lar_2012.sql'
+	\i 'SQL/create_and_load_lar_2013.sql'
+	\i 'SQL/create_and_load_lar_2014.sql'
+	\i 'SQL/create_and_load_lar_2015.sql'
+	\i 'SQL/create_and_load_lar_2016.sql'
+	\i 'SQL/create_and_load_lar_2017.sql'
 
 
 EOF
