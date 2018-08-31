2d8e7ab30b4b22fa89b3d4c13e300855760b34c4
Kibrael Roell <kibrael.roell@cfpb.gov>
2018-08-31 16:06:50 -0400

    fix sql files after overwrite

--- hmda_load_scripts/SQL/create_and_load_panel_2015.sql
+++ hmda_load_scripts/SQL/create_and_load_panel_2015.sql
@@ -1,53 +1,87 @@
-
-DROP TABLE IF EXISTS lar_2015;
-CREATE TABLE lar_2015 (  
-    year VARCHAR,
+DROP TABLE IF EXISTS panel_2015;
+CREATE TABLE panel_2015 (
+    activity_year VARCHAR,
     respondent_id VARCHAR,
     agency_code VARCHAR,
-    loan_type VARCHAR,
-    property_type VARCHAR,
-    loan_purpose VARCHAR,
-    occupancy VARCHAR,
-    loan_amount VARCHAR,
-    preapproval VARCHAR,
-    action_type VARCHAR,
-    msa VARCHAR,
-    state_code VARCHAR,
-    county_code VARCHAR,
-    census_tract VARCHAR,
-    applicant_ethnicity VARCHAR,
-    co_applicant_ethnicity VARCHAR,
-    applicant_race_1 VARCHAR,
-    applicant_race_2 VARCHAR,
-    applicant_race_3 VARCHAR,
-    applicant_race_4 VARCHAR,
-    applicant_race_5 VARCHAR,
-    co_applicant_race_1 VARCHAR,
-    co_applicant_race_2 VARCHAR,
-    co_applicant_race_3 VARCHAR,
-    co_applicant_race_4 VARCHAR,
-    co_applicant_race_5 VARCHAR,
-    applicant_sex VARCHAR,
-    co_applicant_sex VARCHAR,
-    income VARCHAR,
-    purchaser_type VARCHAR,
-    denial_1 VARCHAR,
-    denial_2 VARCHAR,
-    denial_3 VARCHAR,
-    rate_spread VARCHAR,
-    hoepa_status VARCHAR,
-    lien_status VARCHAR,
-    edit_status VARCHAR,
-    sequence_num VARCHAR,
-    population VARCHAR,
-    minority_population_percent VARCHAR,
-    ffiec_median_income VARCHAR,
-    tract_to_msa_income_percent VARCHAR,
-    owner_occupied_units VARCHAR,
-    one_to_four_units VARCHAR,
-    app_date_indicator VARCHAR
-)
-
-COPY lar_2015
-FROM '{data_path}HMDA_Data_Science_Kit/data/lar/2015HMDALAR - National.csv'
-    DELIMITER ',' ENCODING 'latin1';
+    parent_respondent_id VARCHAR,
+    parent_name VARCHAR,
+    filler VARCHAR,
+    parent_city VARCHAR,
+    parent_state VARCHAR,
+    region VARCHAR,
+    assets VARCHAR,
+    other_lender_code VARCHAR,
+    respondent_name VARCHAR,
+    respondent_city VARCHAR,
+    respondent_state VARCHAR,
+    filler2 VARCHAR,
+    filler3 VARCHAR,
+    top_holder_rssd VARCHAR,
+    top_holder_name VARCHAR,
+    top_holder_city VARCHAR,
+    top_holder_state VARCHAR,
+    top_holder_country VARCHAR,
+    respondent_rssd VARCHAR,
+    parent_rssd VARCHAR,
+    respondent_state_fips VARCHAR);
+ CREATE TEMPORARY TABLE panel_load
+    (PANEL VARCHAR); -- LAR contains an entire LAR record
+ COPY panel_load
+FROM '{data_path}HMDA_Data_Science_Kit/data/panel/panel_2015.txt' 
+    ENCODING 'latin1';
+COMMIT;
+ INSERT INTO panel_2015 (
+    activity_year,
+    resp_id,
+    agency,
+    parent_resp_id,
+    parent_name,
+    parent_city,
+    parent_state,
+    region,
+    assets,
+    olc,
+    resp_name,
+    filler,
+    resp_city,
+    resp_state,
+    filler2,
+    filler3,
+    top_holder_rssd,
+    top_holder_name,
+    top_holder_city,
+    top_holder_state,
+    top_holder_country,
+    resp_rssd,
+    parent_rssd,
+    resp_state_fips
+    )
+ SELECT 
+    SUBSTRING(PANEL, 1, 4),
+    SUBSTRING(PANEL, 5, 10),
+    SUBSTRING(PANEL, 15, 1),
+    SUBSTRING(PANEL, 16, 10),
+    SUBSTRING(PANEL, 26, 30),
+    SUBSTRING(PANEL, 56, 80),
+    SUBSTRING(PANEL, 81, 2),
+    SUBSTRING(PANEL, 83, 2),
+    SUBSTRING(PANEL, 85, 10),
+    SUBSTRING(PANEL, 95, 1),
+    SUBSTRING(PANEL, 96, 30),
+    SUBSTRING(PANEL, 126, 40),
+    SUBSTRING(PANEL, 166, 25),
+    SUBSTRING(PANEL, 191, 2),
+    SUBSTRING(PANEL, 193, 10),
+    SUBSTRING(PANEL, 203, 10),
+    SUBSTRING(PANEL, 213, 10),
+    SUBSTRING(PANEL, 223, 30),
+    SUBSTRING(PANEL, 253, 25),
+    SUBSTRING(PANEL, 278, 2),
+    SUBSTRING(PANEL, 280, 40),
+    SUBSTRING(PANEL, 320, 10),
+    SUBSTRING(PANEL, 330, 10),
+    SUBSTRING(PANEL, 340, 2)
+ FROM panel_load;
+COMMIT;
+DROP TABLE IF EXISTS panel_load; 
+COMMIT; 
