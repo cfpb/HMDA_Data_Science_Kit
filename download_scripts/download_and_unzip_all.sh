256f252e68997bc51918b6dc86395c8331d36be7
Kibrael Roell <kibrael.roell@cfpb.gov>
2018-09-04 14:42:01 -0400

    change download file paths to match instructions

--- download_scripts/download_and_unzip_all.sh
+++ download_scripts/download_and_unzip_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-bash download_ts_2004_2017.sh
-bash download_panel_2004_2017.sh
-bash download_lar_2004_2017.sh
+bash download_scripts/download_ts_2004_2017.sh
+bash download_scripts/download_panel_2004_2017.sh
+bash download_scripts/download_lar_2004_2017.sh
 
-bash unzip_and_rename_lar.sh
+bash download_scripts/unzip_and_rename_lar.sh
--- download_scripts/unzip_and_rename_lar.sh
+++ download_scripts/unzip_and_rename_lar.sh
@@ -1,29 +1,29 @@
 #unzip and rename files
-unzip ../data/lar/lar_2016.zip -d ../data/lar 
-unzip ../data/lar/lar_2015.zip -d ../data/lar 
-unzip ../data/lar/lar_2014.zip -d ../data/lar 
-unzip ../data/lar/Lars.ultimate.2013.dat.zip -d ../data/lar 
-unzip ../data/lar/Lars.ultimate.2012.dat.zip -d ../data/lar 
-unzip ../data/lar/Lars.ultimate.2011.dat.zip -d ../data/lar 
-unzip ../data/lar/Lars.ultimate.2010.dat.zip -d ../data/lar 
-unzip ../data/lar/2009_Ultimate_PUBLIC_LAR.dat.zip -d ../data/lar 
-unzip ../data/lar/Lars.ultimate.2008.dat.zip -d ../data/lar 
-unzip ../data/lar/Lars.ultimate.2007.dat.zip -d ../data/lar 
-unzip ../data/lar/Lars.ultimate.2006.dat.zip -d ../data/lar 
-unzip ../data/lar/LARS.ULTIMATE.2005.DAT.zip -d ../data/lar 
-unzip ../data/lar/u2004lar.public.dat.zip -d ../data/lar 
+unzip data/lar/lar_2016.zip -d data/lar 
+unzip data/lar/lar_2015.zip -d data/lar 
+unzip data/lar/lar_2014.zip -d data/lar 
+unzip data/lar/Lars.ultimate.2013.dat.zip -d data/lar 
+unzip data/lar/Lars.ultimate.2012.dat.zip -d data/lar 
+unzip data/lar/Lars.ultimate.2011.dat.zip -d data/lar 
+unzip data/lar/Lars.ultimate.2010.dat.zip -d data/lar 
+unzip data/lar/2009_Ultimate_PUBLIC_LAR.dat.zip -d data/lar 
+unzip data/lar/Lars.ultimate.2008.dat.zip -d data/lar 
+unzip data/lar/Lars.ultimate.2007.dat.zip -d data/lar 
+unzip data/lar/Lars.ultimate.2006.dat.zip -d data/lar 
+unzip data/lar/LARS.ULTIMATE.2005.DAT.zip -d data/lar 
+unzip data/lar/u2004lar.public.dat.zip -d data/lar 
 
 
-#mv ../data/lar/lar_2016.txt
-#mv ../data/lar/lar_2015.txt
-#mv ../data/lar/lar_2014.txt
-mv ../data/lar/Lars.ultimate.2013.dat ../data/lar/lar_ult_2013.dat
-mv ../data/lar/Lars.ultimate.2012.dat ../data/lar/lar_ult_2012.dat
-mv ../data/lar/Lars.ultimate.2011.dat ../data/lar/lar_ult_2011.dat
-mv ../data/lar/Lars.ultimate.2010.dat ../data/lar/lar_ult_2010.dat
-mv ../data/lar/2009_Ultimate_PUBLIC_LAR.dat ../data/lar/lar_ult_2009.dat
-mv ../data/lar/Lars.ultimate.2008.dat ../data/lar/lar_ult_2008.dat
-mv ../data/lar/Lars.ultimate.2007.dat ../data/lar/lar_ult_2007.dat
-mv ../data/lar/Lars.ultimate.2006.dat ../data/lar/lar_ult_2006.dat
-mv ../data/lar/LARS.ULTIMATE.2005.DAT ../data/lar/lar_ult_2005.dat
-mv ../data/lar/u2004lar.public.dat ../data/lar/lar_ult_2004.dat
+#mv data/lar/lar_2016.txt
+#mv data/lar/lar_2015.txt
+#mv data/lar/lar_2014.txt
+mv data/lar/Lars.ultimate.2013.dat data/lar/lar_ult_2013.dat
+mv data/lar/Lars.ultimate.2012.dat data/lar/lar_ult_2012.dat
+mv data/lar/Lars.ultimate.2011.dat data/lar/lar_ult_2011.dat
+mv data/lar/Lars.ultimate.2010.dat data/lar/lar_ult_2010.dat
+mv data/lar/2009_Ultimate_PUBLIC_LAR.dat data/lar/lar_ult_2009.dat
+mv data/lar/Lars.ultimate.2008.dat data/lar/lar_ult_2008.dat
+mv data/lar/Lars.ultimate.2007.dat data/lar/lar_ult_2007.dat
+mv data/lar/Lars.ultimate.2006.dat data/lar/lar_ult_2006.dat
+mv data/lar/LARS.ULTIMATE.2005.DAT data/lar/lar_ult_2005.dat
+mv data/lar/u2004lar.public.dat data/lar/lar_ult_2004.dat
