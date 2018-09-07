06dbfb00c1eae4c7c5cb0c291d38bf0d2d1808e2
Kibrael Roell <kibrael.roell@cfpb.gov>
2018-09-07 11:52:52 -0400

    remove download function from unzip script

--- download_scripts/download_and_unzip_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-bash download_scripts/download_ts_2004_2017.sh
-bash download_scripts/download_panel_2004_2017.sh
-bash download_scripts/download_lar_2004_2017.sh
-
-bash download_scripts/unzip_and_rename_lar.sh
-bash download_scripts/unzip_ts.sh
-bash download_scripts/unzip_panel.sh
---
+++ download_scripts/unzip_all.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+bash download_scripts/unzip_and_rename_lar.sh
+bash download_scripts/unzip_ts.sh
+bash download_scripts/unzip_panel.sh
