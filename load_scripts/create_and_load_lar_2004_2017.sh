8455c3546973caf99d3efa8907a58031f74241f8
Kibrael Roell <kibrael.roell@cfpb.gov>
2018-09-07 17:36:09 -0400

    add option flags for lar load script

--- load_scripts/create_and_load_lar_2004_2017.sh
+++ load_scripts/create_and_load_lar_2004_2017.sh
@@ -1,19 +1,67 @@
 #!/bin/bash
+#Set default PSQL connection variables
+USER="postgres" #the postgres username for the connection
+PASSWORD="" #user's password for the connection
+DATABASE="postgres" #Sets connection database
+HOST="localhost" #host address of the database
+PORT="5432" #connection port of the database
 
-set -e
-set -u
+if [ $# -gt 0 ]; then
+	#while [ "${1}" != "" ]; 
+	for var in "$@"
+	do #iterate over passed command line arguments as long as one remains
+	    case $1 in
+	        -u | --user )           shift
+	                                USER=$1
+	                                echo "user: $USER"
+	                                ;;
+
+	        -p | --password )       shift
+									PASSWORD=$1
+	                                ;;
+
+	        -d | --database )		shift
+									DATABASE=$1
+									echo "database: $DATABASE"
+									;;
+
+			-h | --host )			shift
+									HOST=$1
+									echo "HOST: $HOST"
+									;;
+
+			-o | --port ) 			shift
+									PORT=$1
+									;;
+
+	        --help )           		echo
+									echo "*****************"
+									echo "This section explains the command line arguments available in this script"
+									echo ""
+									echo "-u <username> or --user <username> sets the username for connection to database"
+									echo "-p <password> or --password <password> sets the password for the connection"
+									echo "-d <database> or --database <database> sets the database for connection"
+									echo "-h <host address> or --host <host address> sets the connection address for the database"
+									echo "-o <port> or --port <port> sets the connection port for the database"
+									echo "--help calls this output"
+	                                exit
+	                                ;;
+	    esac
+	done
+fi
 
 # Set these environmental variables to override them,
 # but they have safe defaults.
-export PGHOST=${PGHOST-localhost}
-export PGPORT=${PGPORT-5432} #default port for localhost
-export PGDATABASE=${PGDATABASE-postgres} #initial connection database, HMDA db will be created in this script
-export PGUSER=${PGUSER-postgres} #enter your PG username
-export PGPASSWORD=${PGPASSWORD-} #PG password
+export PGUSER="${PGUSER-${USER}}" #postgres user for the connection
+export PGPASSWORD="${PGPASSWORD-${PASSWORD}}" #PG password for the user
+export PGDATABASE="${PGDATABASE-${DATABASE}}" #Sets connection database
+export PGHOST="${PGHOST-${HOST}}" #host address of the database
+export PGPORT="${PGPORT-${PORT}}" #connection port of the database
 
+echo "setting data paths for SQL load"
 python3 hmda_load_scripts/write_data_paths.py
 
-echo "creating HMDA tables for 2004-2017"
+echo "creating HMDA LAR tables for 2004-2017"
 psql $PGUSER $PGDATABASE << EOF
 	\c hmda;
 	set schema 'hmda_public';
@@ -31,6 +79,4 @@ psql $PGUSER $PGDATABASE << EOF
 	\i 'load_scripts/SQL/create_and_load_lar_2015.sql'
 	\i 'load_scripts/SQL/create_and_load_lar_2016.sql'
 	\i 'load_scripts/SQL/create_and_load_lar_2017.sql'
-
-
 EOF
