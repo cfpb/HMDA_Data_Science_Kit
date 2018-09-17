368fb0f0cc75ebf18742961b7921eacb2fe6c451
Kibrael Roell <kibrael.roell@cfpb.gov>
2018-09-17 14:40:13 -0400

    change TS formate from dynamic to snapshot

--- load_scripts/SQL/create_and_load_ts_2017.sql
+++ load_scripts/SQL/create_and_load_ts_2017.sql
@@ -1,11 +1,9 @@
 DROP TABLE IF EXISTS ts_2017;
 CREATE TABLE ts_2017 (
-	record_id VARCHAR,
+	activity_year VARCHAR,
 	respondent_id VARCHAR,
 	agency_code VARCHAR,
-	activity_year VARCHAR,
 	tax_id VARCHAR,
-	lar_entries VARCHAR,
 	respondent_name VARCHAR,
 	respondent_address VARCHAR,
 	respondent_city VARCHAR,
@@ -15,11 +13,12 @@ CREATE TABLE ts_2017 (
 	parent_address VARCHAR,
 	parent_city VARCHAR,
 	parent_state VARCHAR,
-	parent_zip VARCHAR
+	parent_zip VARCHAR,
+	lar_count VARCHAR
 	);
 
 COPY ts_2017
-FROM --'{data_path}HMDA_Data_Science_Kit/data/ts/ts_2017.txt'
+FROM 
 '{data_path}HMDA_Data_Science_Kit/data/ts/ts_2017.txt'
     DELIMITER '|' ENCODING 'latin1';
 COMMIT;
