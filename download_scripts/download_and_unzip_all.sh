0e5a93e5045ef4ee821909269ac8d55900dbc544
Kibrael Roell <kibrael.roell@cfpb.gov>
2018-09-17 12:51:52 -0400

    remove old download script

--- download_scripts/download_and_unzip_all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#This file unzips all compressed files in the data/lar, data/ts, and data/panel directories.
-#Files are renamed to match the schema <hmda_dataset_hmda_year>.<extension> format.
-bash download_scripts/unzip_and_rename_lar.sh
-bash download_scripts/unzip_ts.sh
-bash download_scripts/unzip_panel.sh
